a6f915216f02b09fe1bc29a7d9b6b692f14563bd
Kalpit Khamar <kkk0482@njit.edu>
2019-06-23 18:45:16 -0400

    Fix: master and origin branch oush test

--- index.php
+++ index.php
@@ -6,5 +6,12 @@
  * Time: 6:27 PM
  */
 
-//main::start("example.csv");
+main::start("example.csv");
 echo 'test 101 kalpit khamar';
+
+class main  {
+    static public function start($filename) {
+        $records = csv::getRecords($filename);
+        $table = html::generateTable($records);
+    }
+}
